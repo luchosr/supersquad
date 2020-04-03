b6b176057f06d8987efaca01ca9a4fe0b3222a60
Luciano <luchosr@gmail.com>
2020-04-03 17:28:14 -0300

    add new folder structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Supersquad</h2>
+    </div>
+  );
 }
 
 export default App;
