127a701ea1b678c2c1291c2806b07b840fcac434
Luciano <luchosr@gmail.com>
2020-04-04 13:10:29 -0300

    updates actions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import { addCharacterById } from "./actions";
+
+const store = createStore(rootReducer);
+console.log("store.getState() es ", store.getState());
+store.subscribe(() => console.log("store ", store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
